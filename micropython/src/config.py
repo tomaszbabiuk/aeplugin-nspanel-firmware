8fbbac4a495a11e0a1215c97c7f773ab42bc8134
Tomasz Babiuk <tomasz.babiuk@gmail.com>
2023-01-22 19:45:38 +0100

    adjustments to new setup.tft

---
+++ micropython/src/config.py
@@ -0,0 +1,58 @@
+import json
+from micropython import const
+
+Config_Filename = const("config.json")
+Field_Language = const("lang")
+Field_SSID = const("ssid")
+Field_Password = const("password")
+Field_LeftButtonAction = const("leftButtonAction")
+Field_LeftButtonTimeInSec = const("leftButtonTimeInSec")
+Field_RightButtonAction = const("rightButtonAction")
+Field_RightButtonTimeInSec = const("rightButtonTimeInSec")
+
+
+class ConfigManager:
+    def __init__(self) -> None:
+        self.password = ""
+        self.ssid = ""
+        self.lang = 0
+        self.leftButtonAction = 0
+        self.leftButtonTime = 0
+        self.rightButtonAction = 0
+        self.rightButtonTime = 0
+
+    def setPassword(self, password: bytearray) -> None:
+        self.password = password
+
+    def setSsid(self, ssid: bytearray) -> None:
+        self.ssid = ssid
+
+    def setLanguage(self, lang: int) -> None:
+        self.lang = lang
+
+    def setLeftButtonConfig(self, action: int, time: int) -> None:
+        self.leftButtonAction = action
+        self.leftButtonTime = time
+
+    def setRightButtonConfig(self, action: int, time: int) -> None:
+        self.rightButtonAction = action
+        self.rightButtonTime = time
+
+    def save(self) -> None:
+        config = { Field_Language: self.lang, Field_SSID: str(self.ssid), Field_Password: str(self.password), Field_LeftButtonAction: self.leftButtonAction, Field_LeftButtonTimeInSec: self.leftButtonTime, Field_RightButtonAction: self.rightButtonAction, Field_RightButtonTimeInSec: self.rightButtonTime }
+
+        with open(Config_Filename, "w") as config_file:
+            json.dump(config, config_file)
+        pass
+
+    def load(self):
+        with open(Config_Filename, "r") as config_file:
+            config = json.load(config_file)
+            print(config)
+            # print("Language: {}".format(config[Field_Language]))
+            # print("SSID: {}".format(config[Field_Language]))
+            # print("Password: {}".format(config[Field_Language]))
+            # print("LeftButtonAction: {}".format(config[Field_Language]))
+            # print("LeftButtonTime: {}".format(config[Field_Language]))
+            # print("RightButtonAction: {}".format(config[Field_Language]))
+            # print("LeftButtonTime: {}".format(config[Field_Language]))
